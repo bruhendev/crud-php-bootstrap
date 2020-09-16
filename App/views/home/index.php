4a582cc1715ed80c7b53c9e620b7f758d6ddcf91
Bruno Henrique <brunohenrique242@gmail.com>
2020-09-16 18:11:15 -0300

    add tabela no template

--- App/views/home/index.php
+++ App/views/home/index.php
@@ -1,4 +1,58 @@
-<?php
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1 class="h2 my-2">Lista de Usuarios</h1>
+        </div>
+        <div class="col-12">
+            <div class="col mb-2">
+                <a class="btn btn-primary text-white">Novo Usuário</a>
+            </div>
+            <table class="col table table-hover table-sm">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Telefone</th>
+                        <th scope="col">Administrador</th>
+                        <th scope="col">Sistuação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Bruno</td>
+                        <td>brunohenrique@email.com</td>
+                        <td>(94)91919191</td>
+                        <td>Sim</td>
+                        <td>Ativo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Carlos</td>
+                        <td>carlos@email.com</td>
+                        <td>(94)91919191</td>
+                        <td>Sim</td>
+                        <td>Ativo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Diego</td>
+                        <td>diego@email.com</td>
+                        <td>(94)91919191</td>
+                        <td>Sim</td>
+                        <td>Ativo</td>
+                    </tr>
+                </tbody>
+            </table>
 
-echo '<h1>Pagina Principal</h1>';
+        </div>
+
+    </div>
+
+
+</div>
+
+<?php
+echo '';
 
